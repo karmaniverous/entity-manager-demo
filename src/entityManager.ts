21031a570b75ee393ef287fdd2df761a9c0e0e09
Jason Williscroft <me@karmanivero.us>
2024-10-31 15:48:48 +0800

    updated comments

--- src/entityManager.ts
+++ src/entityManager.ts
@@ -5,6 +5,7 @@ import {
 } from '@karmaniverous/entity-manager';
 import type { Entity } from '@karmaniverous/entity-tools';
 
+// Email interface. never types indicate generated properties.
 interface Email extends Entity {
   created: number;
   email: string;
@@ -12,6 +13,7 @@ interface Email extends Entity {
   userHashKey: never; // generated
 }
 
+// User interface. never types indicate generated properties.
 interface User extends Entity {
   beneficiaryId: string;
   created: number;
@@ -28,22 +30,42 @@ interface User extends Entity {
   userHashKey: never; // generated
 }
 
+// Entity interfaces combined into EntityMap.
 interface MyEntityMap extends EntityMap {
   email: Email;
   user: User;
 }
 
+// Current timestamp will act as break point for sharding schedule.
+const now = Date.now();
+
+// Config object for EntityManager.
+// Using default values for HashKey, RangeKey, and TranscodeMap type params.
 const config: Config<MyEntityMap> = {
+  // Common hash & range key properties for all entities. Must exactly match
+  // HashKey & RangeKey type params.
+  hashKey: 'hashKey',
+  rangeKey: 'rangeKey',
+
+  // Entity-specific configs. Keys must exactly match those of MyEntityMap.
   entities: {
+    // Email entity config.
     email: {
-      elementTranscodes: {
-        created: 'timestamp',
-        userId: 'string',
-      },
-      indexes: {
-        created: ['hashKey', 'rangeKey', 'created'],
-        userCreated: ['hashKey', 'rangeKey', 'userHashKey', 'created'],
-      },
+      // Source property for the Email entity's hash key.
+      uniqueProperty: 'email',
+
+      // Source property for timestamp used to calculate Email shard key.
+      timestampProperty: 'created',
+
+      // Email entity's shard bump schedule.
+      // Records created before now are unsharded (1 possible shard key).
+      // Records created after now have a 1-char, 2-bit shard key (4 possible shard keys).
+      shardBumps: [{ timestamp: now, charBits: 2, chars: 1 }],
+
+      // Email entity generated properties.
+      // These keys must match the ones with never types in the Email interface.
+      // These properties are marked with a ⚙️ in the table design at
+      // https://karmanivero.us/projects/entity-manager/evolving-a-nosql-db-schema/#table-properties
       generated: {
         userHashKey: {
           atomic: true,
@@ -51,18 +73,46 @@ const config: Config<MyEntityMap> = {
           sharded: true,
         },
       },
-      timestampProperty: 'created',
-      uniqueProperty: 'email',
-    },
-    user: {
+      indexes: {
+        created: ['hashKey', 'rangeKey', 'created'],
+        userCreated: ['hashKey', 'rangeKey', 'userHashKey', 'created'],
+      },
       elementTranscodes: {
-        beneficiaryId: 'string',
         created: 'timestamp',
-        firstNameCanonical: 'string',
-        lastNameCanonical: 'string',
-        phone: 'string',
-        updated: 'timestamp',
         userId: 'string',
+      },
+    },
+    // User entity config.
+    user: {
+      // Source property for the User entity's hash key.
+      uniqueProperty: 'userId',
+
+      // Source property for timestamp used to calculate User shard key.
+      timestampProperty: 'created',
+
+      // User entity's shard bump schedule.
+      // Records created before now are unsharded (1 possible shard key).
+      // Records created after now have a 1-char, 2-bit shard key (4 possible shard keys).
+      shardBumps: [{ timestamp: now, charBits: 2, chars: 1 }],
+      generated: {
+        firstNameRangeKey: {
+          atomic: true,
+          elements: ['firstNameCanonical', 'lastNameCanonical', 'created'],
+        },
+        lastNameRangeKey: {
+          atomic: true,
+          elements: ['lastNameCanonical', 'firstNameCanonical', 'created'],
+        },
+        userBeneficiaryHashKey: {
+          atomic: true,
+          elements: ['beneficiaryId'],
+          sharded: true,
+        },
+        userHashKey: {
+          atomic: true,
+          elements: ['userId'],
+          sharded: true,
+        },
       },
       indexes: {
         created: ['hashKey', 'rangeKey', 'created'],
@@ -101,32 +151,17 @@ const config: Config<MyEntityMap> = {
           'updated',
         ],
       },
-      generated: {
-        firstNameRangeKey: {
-          atomic: true,
-          elements: ['firstNameCanonical', 'lastNameCanonical', 'created'],
-        },
-        lastNameRangeKey: {
-          atomic: true,
-          elements: ['lastNameCanonical', 'firstNameCanonical', 'created'],
-        },
-        userBeneficiaryHashKey: {
-          atomic: true,
-          elements: ['beneficiaryId'],
-          sharded: true,
-        },
-        userHashKey: {
-          atomic: true,
-          elements: ['userId'],
-          sharded: true,
-        },
+      elementTranscodes: {
+        beneficiaryId: 'string',
+        created: 'timestamp',
+        firstNameCanonical: 'string',
+        lastNameCanonical: 'string',
+        phone: 'string',
+        updated: 'timestamp',
+        userId: 'string',
       },
-      timestampProperty: 'created',
-      uniqueProperty: 'userId',
     },
   },
-  hashKey: 'hashKey',
-  rangeKey: 'rangeKey',
 };
 
 export const entityManager = new EntityManager(config);
