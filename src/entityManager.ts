2db85233bd2597315505a2f3ea1b479464906c36
Jason Williscroft <me@karmanivero.us>
2024-10-31 17:32:10 +0800

    finished commenting entityManager config

--- src/entityManager.ts
+++ src/entityManager.ts
@@ -40,42 +40,73 @@ interface MyEntityMap extends EntityMap {
 const now = Date.now();
 
 // Config object for EntityManager.
-// Using default values for HashKey, RangeKey, and TranscodeMap type params.
+// Using default values for HashKey, RangeKey, and TranscodeMap
+// type params.
 const config: Config<MyEntityMap> = {
-  // Common hash & range key properties for all entities. Must exactly match
-  // HashKey & RangeKey type params.
+  // Common hash & range key properties for all entities. Must
+  // exactly match HashKey & RangeKey type params.
   hashKey: 'hashKey',
   rangeKey: 'rangeKey',
 
-  // Entity-specific configs. Keys must exactly match those of MyEntityMap.
+  // Entity-specific configs. Keys must exactly match those of
+  // MyEntityMap.
   entities: {
     // Email entity config.
     email: {
       // Source property for the Email entity's hash key.
       uniqueProperty: 'email',
 
-      // Source property for timestamp used to calculate Email shard key.
+      // Source property for timestamp used to calculate Email
+      // shard key.
       timestampProperty: 'created',
 
-      // Email entity's shard bump schedule.
-      // Records created before now are unsharded (1 possible shard key).
-      // Records created after now have a 1-char, 2-bit shard key (4 possible shard keys).
+      // Email entity's shard bump schedule. Records created before
+      // now are unsharded (1 possible shard key). Records created
+      // after now have a 1-char, 2-bit shard key (4 possible shard
+      // keys).
       shardBumps: [{ timestamp: now, charBits: 2, chars: 1 }],
 
-      // Email entity generated properties.
-      // These keys must match the ones with never types in the Email interface.
-      // These properties are marked with a ⚙️ in the table design at https://karmanivero.us/projects/entity-manager/evolving-a-nosql-db-schema/#table-properties
+      // Email entity generated properties. These keys must match
+      // the ones with never types in the Email interface defined
+      // above, and are marked with a ⚙️ in the table design.
       generated: {
         userHashKey: {
+          // When true, if any element is undefined or null, the
+          // generated property will be undefined. When false,
+          // undefined or null elements will be rendered as an
+          // empty string.
           atomic: true,
+
+          // Elements of the generated property. These MUST be
+          // ungenerated properties (i.e. not marked with never
+          // in the Email interface) and MUST be included in the
+          // entityTranscodes object below. Elements are applied
+          // in order.
           elements: ['userId'],
+
+          // When this value is true, the generated property will
+          // be sharded.
           sharded: true,
         },
       },
+
+      // Indexes for the Email entity as specified in the index
+      // design.
       indexes: {
+        // Index components can be any combination of hashKey,
+        // rangeKey, generated properties, and ungenerated
+        // properties. Any ungenerated properties MUST be included
+        // in the entityTranscodes object below. Property order is
+        // not significant.
         created: ['hashKey', 'rangeKey', 'created'],
         userCreated: ['hashKey', 'rangeKey', 'userHashKey', 'created'],
       },
+
+      // Transcodes for ungenerated properties used as generated
+      // property elements or index components. Transcode values
+      // must be valid config transcodes object keys. Since this
+      // config does not define a transcodes object it uses
+      // defaultTranscodes exported by @karmaniverous/entity-tools.
       elementTranscodes: {
         created: 'timestamp',
         userId: 'string',
@@ -83,15 +114,8 @@ const config: Config<MyEntityMap> = {
     },
     // User entity config.
     user: {
-      // Source property for the User entity's hash key.
       uniqueProperty: 'userId',
-
-      // Source property for timestamp used to calculate User shard key.
       timestampProperty: 'created',
-
-      // User entity's shard bump schedule.
-      // Records created before now are unsharded (1 possible shard key).
-      // Records created after now have a 1-char, 2-bit shard key (4 possible shard keys).
       shardBumps: [{ timestamp: now, charBits: 2, chars: 1 }],
       generated: {
         firstNameRangeKey: {
@@ -163,4 +187,5 @@ const config: Config<MyEntityMap> = {
   },
 };
 
+// Configure & export EntityManager instance.
 export const entityManager = new EntityManager(config);
